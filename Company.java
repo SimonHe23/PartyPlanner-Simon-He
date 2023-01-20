cfd9a9d8ff7b0b558cd2f5317f870d97560ed924
SimonHe23 <77286080+SimonHe23@users.noreply.github.com>
2023-01-20 15:36:17 -0500

    Update Company.java

--- Company.java
+++ Company.java
@@ -4,6 +4,11 @@
  * @date 2023-01-19
  */
 
+/**
+ * There are many getter and setter functions in this class, 
+ * where the data types in a List or Array list are split into the desired data types.
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 
